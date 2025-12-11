57c9e064b7580c52ee19af52484e3238a7b8efe8
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2025-12-11 20:24:12 -0300

    [FEAT] Mejora graficos + Aumento complejidad entorno

--- src/main.py
+++ src/main.py
@@ -11,26 +11,25 @@ class Tienda(gym.Env):
         self.max_day = max_day
         self.money = 100
         self.stock = 0
+        self.inventario = []
         self.price = 10
         self.day = 0
         #Acciones posibles, comprar de 0, 10, 20, 30, 40, 50 unidades
         self.action_space = spaces.Discrete(6)
         #Definir observaciones, el agente ve el dinero, precio y stock disponible
         self.observation_space = spaces.Box(
-            low=np.array([0,0,10,0], dtype=np.int32), #Para que no sean negativos
+            low=np.array([0,0,7,0], dtype=np.int32), #Para que no sean negativos
             high=np.array([
-            MAX_DEMANDA*30*10 + 100,          # Dinero máximo estimado
-            1500,           # Stock máximo estimado
-            10,            # Precio máximo
+            6996,          # Dinero máximo estimado
+            552,           # Stock máximo estimado
+            13,            # Precio máximo
             self.max_day   # Día máximo
         ], dtype=np.int32), #No hay limite superior
             shape=(4,), #Cantidad de variables a observar
             dtype=np.int32
         )
 
-
     def step(self, action):
-        
         #Inicializar variables de control por defecto
         terminated = False
         truncated = False
@@ -43,25 +42,60 @@ class Tienda(gym.Env):
         buy_unit = action * 10
         cost = buy_unit * self.price
 
+        # Eliminar los lotes vencidos
+        Cantidad_lotes = len(self.inventario)
+        lotes_vencidos = [l for l in self.inventario if l["dias"] <= 0]
+        self.inventario = [l for l in self.inventario if l["dias"] > 0]
+
+
+
+        # COMPRAR
+
         # Verificamos si tiene dinero suficiente (para no tener saldo negativo)
         if self.money >= cost:
             self.money -= cost
             self.stock += buy_unit
+            nuevo_lote = {
+                "cantidad" : buy_unit,
+                "dias" : 5, # dias para que caduque
+                "precio" : cost
+            }
+            self.inventario.append(nuevo_lote)
         else:
             penalty = 10
             info['compra_fallida'] = True
+
+        # VENDER
+
         demand = np.random.randint(0, MAX_DEMANDA)
         sold = min(self.stock, demand)
         revenue = sold * (self.price * 2)
         self.money += revenue      # Entra dinero
         self.stock -= sold          # Sale stock
-        missed_sales = demand - sold  # Ventas perdidas
+        missed_sales = demand - sold  # Ventas perdidas 
 
+        cantidad_a_vender = sold
+        for lote in self.inventario:
+            if cantidad_a_vender <= 0:
+                break
+            disponible = lote["cantidad"]
+            usado = min(disponible, cantidad_a_vender)
+            lote["cantidad"] -= usado
+            cantidad_a_vender -= usado
+
+        # Eliminar los lotes que se vendieron completamente
+        self.inventario = [lote for lote in self.inventario if lote["cantidad"] > 0]
         #2. OBSERVACIÓN
         if self.day < self.max_day:
             self.day += 1
+            for lote in self.inventario:
+                lote["dias"] = lote["dias"] - 1
         else:
             terminated = True # Dia final
+
+        # Variamos el precio del producto, el agente podra aprender que le conviene comprar barato
+        self.price += np.random.randint(-1,2)
+        self.price = max(7, min(self.price, 13)) # Limitar precio entre 7 y 13
 
         observation = np.array([
             self.money, 
@@ -73,12 +107,16 @@ class Tienda(gym.Env):
         #3. RECOMPENSA
         profit = revenue - cost
         opportunity_penalty = missed_sales * (self.price * 0.5)
+        if len(lotes_vencidos) > 0:
+            lote_loss_penalty = lotes_vencidos[0]["precio"]
+        else:
+            lote_loss_penalty = 0
         storage_penalty = self.stock * (self.price * 0.1)
         if info.get('compra_fallida'):
             penalty = 100
         else:
             penalty = 0
-        reward = profit - opportunity_penalty - storage_penalty - penalty
+        reward = (profit - opportunity_penalty - storage_penalty - penalty - lote_loss_penalty) / 1000
 
         return observation, reward, terminated, truncated, info
 
@@ -107,4 +145,3 @@ class Tienda(gym.Env):
     def render(self):
         print(f"--- Día {self.day} ---")
         print(f"Dinero: {self.money:.2f} | Stock: {self.stock}")
-
--- src/q-learning.py
+++ src/q-learning.py
@@ -1,33 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
 from main import Tienda
 
 MAX_DEMANDA = 30
 
-def run(episodes, is_training=True, render=False):
+def run(episodes, render=False):
 
     env = Tienda(30)
 
     # Divide position and velocity into segments
-    money_space = np.linspace(env.observation_space.low[0], env.observation_space.high[0], 4401)    # Between -1.2 and 0.6
-    stock_space = np.linspace(env.observation_space.low[1], env.observation_space.high[1], 151)   
-    price_space = np.linspace(env.observation_space.low[2], env.observation_space.high[2], 1)    # Between -0.07 and 0.07
+    money_space = np.linspace(env.observation_space.low[0], env.observation_space.high[0], 2333)    # Between -1.2 and 0.6
+    stock_space = np.linspace(env.observation_space.low[1], env.observation_space.high[1], 139)   
+    price_space = np.linspace(env.observation_space.low[2], env.observation_space.high[2], 7)    # Between -0.07 and 0.07
     day_space = np.linspace(env.observation_space.low[3], env.observation_space.high[3], 31)    # Between -0.07 and 0.07
 
-    if(is_training):
-        q = np.zeros((len(money_space), len(stock_space), len(price_space), len(day_space), env.action_space.n)) # init a 20x20x3 array
-    else:
-        f = open('mountain_car.pkl', 'rb')
-        q = pickle.load(f)
-        f.close()
+    q = np.zeros((len(money_space), len(stock_space), len(price_space), len(day_space), env.action_space.n)) # init a 20x20x3 array
 
-    learning_rate_a = 0.1 # alpha or learning rate
-    discount_factor_g = 0.99 # gamma or discount factor.
+    learning_rate_a = 0.01 # alpha or learning rate
+    discount_factor_g = 0.99# gamma or discount factor.
 
-    epsilon = 1         # 1 = 100% random actions
-    epsilon_decay_rate = 2/episodes # epsilon decay rate
+    epsilon = 1.0
+    epsilon_decay = 0.995
+    epsilon_min = 0.05
     rng = np.random.default_rng()   # random number generator
+
+    max_money = 0
+    max_stock = 0
 
     rewards_per_episode = np.zeros(episodes)
 
@@ -35,7 +33,7 @@ def run(episodes, is_training=True, render=False):
         state = env.reset()[0]      # Starting position, starting velocity always 0
         state_m = np.digitize(state[0], money_space) - 1
         state_s = np.digitize(state[1], stock_space) - 1
-        state_p = 0
+        state_p = np.digitize(state[2], price_space) - 1
         state_d = np.digitize(state[3], day_space) - 1
 
         terminated = False          # True when reached goal
@@ -43,7 +41,7 @@ def run(episodes, is_training=True, render=False):
         rewards=0
 
         while not terminated:
-            if is_training and rng.random() < epsilon:
+            if rng.random() < epsilon:
                 action = env.action_space.sample()
             else:
                 action = np.argmax(q[state_m, state_s, state_p, state_d, :])
@@ -51,13 +49,15 @@ def run(episodes, is_training=True, render=False):
             new_state,reward,terminated,_,_ = env.step(action)
             new_state_m = np.digitize(new_state[0], money_space) - 1
             new_state_s = np.digitize(new_state[1], stock_space) - 1
-            new_state_p = 0
+            new_state_p = np.digitize(state[2], price_space) - 1
             new_state_d = np.digitize(new_state[3], day_space) - 1
 
-            if is_training:
-                q[state_m, state_s, state_p, state_d, action] = q[state_m, state_s, state_p, state_d, action] + learning_rate_a * (
-                    reward + discount_factor_g*np.max(q[new_state_m, new_state_s, new_state_p, new_state_d, :]) - q[state_m, state_s, state_p, state_d, action]
-                )
+            
+
+        
+            q[state_m, state_s, state_p, state_d, action] = q[state_m, state_s, state_p, state_d, action] + learning_rate_a * (
+                reward + discount_factor_g*np.max(q[new_state_m, new_state_s, new_state_p, new_state_d, :]) - q[state_m, state_s, state_p, state_d, action]
+            )
 
             state = new_state
             state_m = new_state_m
@@ -70,29 +70,37 @@ def run(episodes, is_training=True, render=False):
             if render:
                 env.render()
 
-        epsilon = max(epsilon - epsilon_decay_rate, 0)
+            if max_money < money_space[state_m]:
+                max_money = money_space[state_m]
+            if max_stock < stock_space[state_s]:
+                max_stock = stock_space[state_s]
+
+
+        epsilon = max(epsilon_min, epsilon * epsilon_decay)
 
         rewards_per_episode[i] = rewards
 
-        print(f'FIN ITERACION {i}')
+        if render:
+            print(f'FIN ITERACION {i + 1}')
 
 
 
     env.close()
 
-    # Save Q table to file
-    if is_training:
-        f = open('mountain_car.pkl','wb')
-        pickle.dump(q, f)
-        f.close()
-
     mean_rewards = np.zeros(episodes)
     for t in range(episodes):
         mean_rewards[t] = np.mean(rewards_per_episode[max(0, t-100):(t+1)])
-    plt.plot(mean_rewards)
-    plt.savefig(f'mountain_car.png')
+
+    plt.plot(mean_rewards, label=f"Recompensa promedio por Meses")
+    plt.axhline(0, color='black', linewidth=0.8, linestyle='--')  # Línea en y=0
+    plt.legend()
+    plt.savefig('store_rewards.png')
+
+    print(max_money, max_stock)
 
 if __name__ == '__main__':
     # run(1000, is_training=True, render=False)
 
-    run(5000, is_training=True, render=True)
+    run(20000, render=True)
+
+    
